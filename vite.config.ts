616e93793db339a9863bc8e1a834b4779fb87b69
dheer152004 <dheerajkumar152004@outlook.com>
2026-01-20 11:23:32 +0530

    Configure for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { exp } from 'firebase/firestore/pipelines';
 
+//for local pc
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        base: '/zentask/',
         port: 3000,
         host: '0.0.0.0',
       },
